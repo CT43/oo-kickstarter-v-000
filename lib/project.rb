588394589bdf474ee9525fd301c18064ad5eb3d8
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-03 18:07:54 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,10 @@
 
-class Project 
-  attr_accessor :title, :backers 
+class Project
+  attr_accessor :title, :backers
 
   def initialize(title)
-    @title = title 
-    @backers = [] 
-  end 
+    @title = title
+    @backers = []
+  end
 
 end 
